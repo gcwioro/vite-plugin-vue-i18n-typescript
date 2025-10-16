2ac1ae934849d9724bc86a7d29aea198f284d4f6
Günther Cwioro <guenther.cwioro@trustbit.tech>
2025-10-16 10:18:01 +0200

    feat: update dependencies and version to 1.1.8, enhance TypeScript configurations

--- example/vite.config.ts
+++ example/vite.config.ts
@@ -4,7 +4,7 @@ import vue from '@vitejs/plugin-vue'
 import viteTsChecker from 'vite-plugin-checker';
 import {join} from "path";
 import {fileURLToPath, URL} from "url";
-// import unpluginVueI18nDtsGeneration from "unplugin-vue-i18n-dts-generation";
+
 import unpluginVueI18nDtsGeneration from "../src";
 // import unpluginVueI18nDtsGeneration, {
 //     VirtualKeysDtsOptions
