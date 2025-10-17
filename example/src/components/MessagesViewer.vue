246cd6a59c1ab1b397edda26a18deddbdafdd259
Günther Cwioro <guenther.cwioro@trustbit.tech>
2025-10-17 19:22:13 +0200

    feat: enhance MessagesViewer layout and styling for improved readability

--- example/src/components/MessagesViewer.vue
+++ example/src/components/MessagesViewer.vue
@@ -1,24 +1,25 @@
 <template>
   <section class="">
     <h2 class="text-lg">Translation Messages ({{ locale }})</h2>
-    <div class="grid gap-4">
-      <div class="label">
+    <div class="sub-section">
+      <p class="label">
         Shows all translation messages using <b>tm('App')</b>
-      </div>
+      </p>
 
       <pre class="code">{{
           JSON.stringify(allMessages, null, 2)
         }}</pre>
     </div>
-    <p class="label">
-      Shows all translation messages using <b>import {useI18nTypeSafe,messages} from
-      "virtual:unplug-i18n-dts-generation";</b>
-    </p>
-    <pre class=" code">
+    <div class="sub-section">
+      <p class="label">
+        Shows all translation messages using <b>import {useI18nTypeSafe,messages} from
+        "virtual:unplug-i18n-dts-generation";</b>
+      </p>
+      <pre class=" code">
 
         {{ JSON.stringify(messages, null, 4) }}
       </pre>
-
+    </div>
 
   </section>
 </template>
